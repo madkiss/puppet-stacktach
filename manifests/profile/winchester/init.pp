17075113577876b02d574f75d9ff3b0c2007b2db
Martin Gerhard Loschwitz <m.loschwitz@syseleven.de>
2015-06-19 13:13:49 +0200

    Initial commit of the Winchester class

---
+++ manifests/profile/winchester/init.pp
@@ -0,0 +1,19 @@
+class stacktach::profile::winchester (
+  $ensure         = $stacktach::profile::params::winchester_package_ensure,
+  $service_manage = $stacktach::profile::params::winchester_service_manage,
+  $service_ensure = $stacktach::profile::params::winchester_service_ensure,
+) inherits stacktach::profile::params {
+   package {"$stacktach::profile::params::winchester_package_name":
+    ensure => $ensure,
+  }
+
+  if $service_manage {
+    service {"$stacktach::profile::params::winchester_service_name":
+      ensure     => $service_ensure,
+      name       => $stacktach::profile::params::winchester_service_name, 
+      enable     => $enabled,
+      hasstatus  => true,
+      hasrestart => true,
+      require    => Package["$stacktach::profile::params::winchester_package_name"],
+  }
+} 
