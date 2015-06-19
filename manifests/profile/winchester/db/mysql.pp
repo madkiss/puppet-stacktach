fbcf3ec73b73f74d76bd23de9a86cbd83909af35
Martin Gerhard Loschwitz <m.loschwitz@syseleven.de>
2015-06-19 13:58:04 +0200

    Add MySQL database creation for Winchester

---
+++ manifests/profile/winchester/db/mysql.pp
@@ -0,0 +1,54 @@
+# The stacktach::profile::winchester::db::mysql class creates a 
+# MySQL database for glance. It must be used on the MySQL server.
+#
+# == Parameters
+#
+#  [*password*]
+#    password to connect to the database. Mandatory.
+#
+#  [*dbname*]
+#    name of the database. Optional. Defaults to winchesterdb.
+#
+#  [*user*]
+#    user to connect to the database. Optional. Defaults to winchester.
+#
+#  [*host*]
+#    the default source host user is allowed to connect from.
+#    Optional. Defaults to 'localhost'
+#
+#  [*allowed_hosts*]
+#    other hosts the user is allowd to connect from.
+#    Optional. Defaults to undef.
+#
+#  [*charset*]
+#    the database charset. Optional. Defaults to 'utf8'
+#
+#  [*collate*]
+#    the database collation. Optional. Defaults to 'utf8_general_ci'
+#
+
+class stacktach::profile::winchester::db::mysql(
+  $password,
+  $dbname                   = 'winchesterdb',
+  $user                     = 'winchester',
+  $host                     = '127.0.0.1',
+  $allowed_hosts            = undef,
+  $charset                  = 'utf8',
+  $collate                  = 'utf8_general_ci',
+  $cluster_id               = 'localzone',
+  $winchester_configuration = '/etc/winchester/winchester.yaml',
+) {
+  validate_string($password)
+
+  ::openstacklib::db::mysql { 'winchesterdb':
+    user          => $user,
+    password_hash => mysql_password($password),
+    dbname        => $dbname,
+    host          => $host,
+    charset       => $charset,
+    collate       => $collate,
+    allowed_hosts => $allowed_hosts,
+  }
+
+  ::Openstacklib::Db::Mysql['winchesterdb'] ~> Exec<| title == "winchester_db -c $winchester_configuration upgrade head" |>
+}
